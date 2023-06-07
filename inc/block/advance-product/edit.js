b964fc2170b85405dda9405c67463aa96188232b
devkushwahshivam <37370154+devkushwahshivam@users.noreply.github.com>
2023-06-07 17:39:32 +0530

    save it

--- inc/block/advance-product/edit.js
+++ inc/block/advance-product/edit.js
@@ -14,6 +14,7 @@ import { omitBy } from 'lodash';
 import hexToRgba from 'hex-rgba';
 import {Fragment,useState,Suspense,useEffect} from '@wordpress/element';
 
+
 /**
  * React hook that is used to mark the block wrapper element.
  * It provides all the necessary props like the class name.
@@ -49,16 +50,17 @@ export default function Edit({ attributes, setAttributes, toggleSelection, clien
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const homeUrl = ThBlockData.homeUrl;
+  const [currentPage, setCurrentPage] = useState(1);
 
   // useEffect hook to fetch product categories
   useEffect(() => {
     // Fetch product categories
     fetch(`${homeUrl}/wp-json/wc/store/v1/products/categories`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setCategories(data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error fetching categories:', error);
       });
   }, []);
@@ -69,47 +71,45 @@ export default function Edit({ attributes, setAttributes, toggleSelection, clien
       setIsLoading(true);
       // Fetch products based on selected category
       fetch(`${homeUrl}/wp-json/wc/store/v1/products?category=${selectedCategory}`)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           setProducts(data);
           setIsLoading(false);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Error fetching products:', error);
         });
     } else {
       setIsLoading(true);
       // Fetch all products if no category is selected
       fetch(`${homeUrl}/wp-json/wc/store/v1/products`)
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
           setProducts(data);
           setIsLoading(false);
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Error fetching products:', error);
         });
     }
-  }, [selectedCategory]);
-
-  // Event handler for tab click
-  const handleTabClick = categoryId => {
-    setSelectedCategory(categoryId);
-  };
+  }, [selectedCategory, currentPage]);
   
-  // show start rating
+  // Event handler for tab click
+  const handleTabClick = (categoryId) => {
+    setSelectedCategory(categoryId);
+    setCurrentPage(1);
+  };
+
+  // Show star rating
   const RatingStars = ({ rating, maxRating = 5, filledColor = 'gold', emptyColor = 'lightgray' }) => {
     const starCount = Math.min(Math.floor(rating), maxRating);
-  
+
     return (
       <div>
         {Array(maxRating)
           .fill()
           .map((_, index) => (
-            <span
-              key={index}
-              style={{ color: index < starCount ? filledColor : emptyColor }}
-            >
+            <span key={index} style={{ color: index < starCount ? filledColor : emptyColor }}>
               &#9733;
             </span>
           ))}
@@ -117,20 +117,21 @@ export default function Edit({ attributes, setAttributes, toggleSelection, clien
     );
   };
 
-  console.log(products);
+  const productsPerPage = 3; // Number of products to display per page
+  const startIndex = (currentPage - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
+  const displayedProducts = products.slice(startIndex, endIndex);
+
   return (
     <div className="th-product-block-wrapper">
       {products.length > 0 ? (
         <>
           <div className="th-product-block-cat-filter">
             <ul className="category-tabs">
-              <li
-                className={!selectedCategory ? 'active' : ''}
-                onClick={() => handleTabClick('')}
-              >
-              All
+              <li className={!selectedCategory ? 'active' : ''} onClick={() => handleTabClick('')}>
+                All
               </li>
-              {categories.map(category => (
+              {categories.map((category) => (
                 <li
                   key={category.id}
                   className={selectedCategory === category.id ? 'active' : ''}
@@ -140,38 +141,59 @@ export default function Edit({ attributes, setAttributes, toggleSelection, clien
                 </li>
               ))}
             </ul>
-          </div> 
-         <div className="th-product-block-product-content">
-        
-           <div className="th-product-block-product-item-wrap">
-           {isLoading && <div className="th-block-loader"></div>}
-            {products.map(product => (
-            <div className="th-product-item" key={product.id}>
-              <div className="th-product-block-content-wrap">
-                <div className="th-product-imgae">
-                {product.on_sale && <span className="sale-tag">Sale</span>}  
-                <div className="th-product-sale"></div>  
-                <img src={product.images[0].thumbnail} alt={product.name} />
+          </div>
+          <div className="th-product-block-product-content">
+            <div className="th-product-block-product-item-wrap">
+              {isLoading && <div className="th-block-loader"></div>}
+              {displayedProducts.map((product) => (
+                <div className="th-product-item" key={product.id}>
+                  <div className="th-product-block-content-wrap">
+                    <div className="th-product-imgae">
+                      {product.on_sale && <span className="sale-tag">Sale</span>}
+                      <div className="th-product-sale"></div>
+                      <img src={product.images[0].thumbnail} alt={product.name} />
+                    </div>
+                    <div className="th-product-cat">
+                      {product.categories.map((category) => (
+                        <a key={category.id} href={category.link}>
+                          {category.name}
+                        </a>
+                      ))}
+                    </div>
+                    <h3 className="th-product-title">
+                      <a key={product.id} href={product.permalink}>
+                        {product.name}
+                      </a>
+                    </h3>
+                    <div
+                      className="th-product-price"
+                      dangerouslySetInnerHTML={{ __html: product.price_html }}
+                    ></div>
+                    <div className="th-product-rating">
+                      <RatingStars rating={parseFloat(product.average_rating)} maxRating={5} filledColor="gold" emptyColor="lightgray" />
+                    </div>
+                    <div className="th-product-btn">{product.add_to_cart.text}</div>
+                  </div>
                 </div>
-                <div className="th-product-cat">
-                {product.categories.map((category) => (
-                 <a key={category.id} href={category.link}>{category.name}</a>
-                ))}
-                </div>
-                <h3 className="th-product-title"><a key={product.id} href={product.permalink}>{product.name}</a></h3>
-                <div className="th-product-price" dangerouslySetInnerHTML={{ __html: product.price_html }}></div>
-                <div className="th-product-rating"><RatingStars rating={parseFloat(product.average_rating)} maxRating={5} filledColor="gold" emptyColor="lightgray" /></div>
-                <div className="th-product-btn">{product.add_to_cart.text}</div>
-              </div>
+              ))}
             </div>
-            ))}
-         </div>
-        </div>
-         </>
-        ) : (
+            <div className="th-pagination">
+              <button disabled={currentPage === 1}
+                onClick={() => setCurrentPage(currentPage - 1)}>
+                          <span className="dashicons dashicons-arrow-left-alt2"></span>
+                        </button>
+                        <button
+                disabled={endIndex >= products.length}
+                onClick={() => setCurrentPage(currentPage + 1)}
+              >
+                <span className="dashicons dashicons-arrow-right-alt2"></span>
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
         <p>Loading products...</p>
-      ) }
-     
+      )}
     </div>
   );
 }
