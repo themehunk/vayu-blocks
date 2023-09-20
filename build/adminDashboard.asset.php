c3d787221f0e63cd45887a4b084f650c64fa9f9c
shivam kushwah <37370154+devkushwahshivam@users.noreply.github.com>
2023-09-20 12:00:35 +0530

    letter spacing issue fixed

---
+++ build/adminDashboard.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => 'c0d509b6e6a7bc00f08b');
---
+++ build/component-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '94bed3b83f07d973fe39');
