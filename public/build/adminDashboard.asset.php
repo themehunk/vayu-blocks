100c8a3f6df413043dc7e2f228ab10c3b5d2f701
iheeresh <heereshd@gmail.com>
2024-03-29 17:43:13 +0530

    readme updated

--- public/build/adminDashboard.asset.php
+++ public/build/adminDashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '1219e4580ddf2489c379');
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '9c665f64174df86f64df');
