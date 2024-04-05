48768b980098ba3985ab319974e5656f3ba0f1f4
iheeresh <heereshd@gmail.com>
2024-04-05 16:54:32 +0530

    json issue resolved

--- public/build/advance-button.asset.php
+++ public/build/advance-button.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'e54c292a2403965fac56');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'c31d1ce65c972fa78d91');
