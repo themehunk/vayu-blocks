622901695ad47ae37946a5475004706fb71fc0ed
ThemeHunk <themehunk@gmail.com>
2024-04-05 17:55:42 +0530

    react react-resux add

--- public/build/advance-heading.asset.php
+++ public/build/advance-heading.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '6bbb299e47777ee73277');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '2456c2719b21dcc5481c');
