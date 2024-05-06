78e2ca4c62a69778af78aecec093dd09a7581b51
ThemeHunk <themehunk@gmail.com>
2024-05-06 17:00:33 +0530

    all demo added in json file

--- public/build/vayu-sites.asset.php
+++ public/build/vayu-sites.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '9bdea7523893867850d2');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'a7c1ceb1df4215731429');
