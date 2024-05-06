9335bf31d4ffd19a71a7efa1e14aed1218d4d222
ThemeHunk <themehunk@gmail.com>
2024-05-06 16:21:01 +0530

    link change

--- public/build/adminDashboard.asset.php
+++ public/build/adminDashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'b76d943429b51cafc672');
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '6ce24a157e07cc788abb');
