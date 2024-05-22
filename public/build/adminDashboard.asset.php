ac92e27e5f1a14fe5a83f0db5943c96ea471c7e4
iheeresh <heereshd@gmail.com>
2024-05-22 13:10:18 +0530

    button color setting modification

--- public/build/adminDashboard.asset.php
+++ public/build/adminDashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'afb71256fdb9b84fbd53');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '52e8e3debf4f429b67e3');
