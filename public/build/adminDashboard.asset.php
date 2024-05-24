791b1fd0cd002db55cf97d7ca1229f3538ac3884
iheeresh <heereshd@gmail.com>
2024-05-24 13:51:25 +0530

    modification

--- public/build/adminDashboard.asset.php
+++ public/build/adminDashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '68cd6cc6974d24fe18d9');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'c51239aeff75a2199070');
