4963fcd567833c7fa199618c0d7fb59a7692b147
ThemeHunk <themehunk@gmail.com>
2024-06-01 16:58:46 +0530

    new demo added

--- public/build/adminDashboard.asset.php
+++ public/build/adminDashboard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => 'c51239aeff75a2199070');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-url'), 'version' => '9cb423956679f539356e');
--- public/build/vayu-sites.asset.php
+++ public/build/vayu-sites.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '879e93b00b4e5b658e3b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '0d5a699f5be2aa0eb40f');
