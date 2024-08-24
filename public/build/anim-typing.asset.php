7b98f5acca70e941315ab8023def6d5a9e2e2cd8
iheeresh <heereshd@gmail.com>
2024-08-24 18:47:33 +0530

    vayu blocks modification

---
+++ public/build/anim-typing.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'f307f5b59a4cfa81288e');
---
+++ public/build/component-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'fbbdb210400ee5633943');
