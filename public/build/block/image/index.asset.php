cf2fb05a1d01f7ca073b3a50380aa566b5b24dab
ThemeHunk <themehunk@gmail.com>
2024-10-16 12:46:01 +0530

    build file added

--- public/build/block/image/index.asset.php
+++ public/build/block/image/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '92b858e7dacef5d9628b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '97175faa92f8307f8ae6');
